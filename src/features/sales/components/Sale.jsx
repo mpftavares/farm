5bc4cc9c92066971a6ec9f420bc3e98c85ea4cd6
mpftavares <marianatavares@live.com.pt>
2023-05-14 13:02:05 +0100

    with fixed sales

--- src/features/sales/components/Sale.jsx
+++ src/features/sales/components/Sale.jsx
@@ -5,11 +5,9 @@ export default function Sale({ sale, index }) {
 	let { id, title, imageUrl, description } = sale;
 	let dispatch = useDispatch();
 
-	const flexDirection = index % 2 === 0 ? 'row' : 'row-reverse';
-
 	return (
 		<section
-			className={`container light-background padding card card-sales ${flexDirection}`}>
+			className={`container light-background padding card card-sales`}>
 			<article className="description">{description}</article>
 			<img
 				src={`images/sales/${imageUrl}`}
