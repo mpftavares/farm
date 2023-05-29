72fce14f8c8c9db9fba353f7ef4c0a29e2d21f2f
mpftavares <marianatavares@live.com.pt>
2023-05-29 11:08:56 +0100

    with sale img transition

--- src/components/sales/Sale.jsx
+++ src/components/sales/Sale.jsx
@@ -1,15 +1,16 @@
 import { useDispatch } from 'react-redux';
 import { openModal, setSelectedSale } from '../../app/saleSlice';
 
-export default function Sale({sale}) {
+export default function Sale({ sale }) {
 	let { id, title, imageUrl, description } = sale;
 	let dispatch = useDispatch();
 
 	return (
 		<article className={`box light-background sale-card padding-2`}>
 			<p className="description">{description}</p>
-			<figure className="card-img center-content">
+			<figure className="card-img center-content radius">
 				<img
+					className="radius"
 					src={`images/sales/${imageUrl}`}
 					onClick={() => {
 						dispatch(setSelectedSale(id));
