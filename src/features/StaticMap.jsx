09b96cf2964ad7da590a96dc9674666c5c694c22
mpftavares <marianatavares@live.com.pt>
2023-07-03 09:14:56 +0100

    with encodeURIComponent

--- src/features/StaticMap.jsx
+++ src/features/StaticMap.jsx
@@ -2,15 +2,14 @@ import { getDirections } from '../app/directions';
 
 export default function StaticMap() {
 	const apiKey = 'AIzaSyCxosBXe5qZ96cgCqV9966gIZ8Js0wQXhU';
-
+	const markerLabel = encodeURIComponent('Farmácia Patuleia');
+  
 	return (
-		<section
-			className="box static-map clickable"
-			onClick={getDirections}>
-			<img
-				className="radius"
-				src={`https://maps.googleapis.com/maps/api/staticmap?center=38.773439,-9.161105&zoom=14&scale=2&size=1200x250&markers=size:mid%7Ccolor:red%7Clabel:%7CFarm%C3%A1cia%20Patuleia&key=${apiKey}`}
-			/>
-		</section>
+	  <section className="box static-map clickable" onClick={getDirections}>
+		<img
+		  className="radius"
+		  src={`https://maps.googleapis.com/maps/api/staticmap?center=38.773439,-9.161105&zoom=14&scale=2&size=1200x250&markers=size:mid%7Ccolor:red%7Clabel:%7C${markerLabel}&key=${apiKey}`}
+		/>
+	  </section>
 	);
-}
+  }
